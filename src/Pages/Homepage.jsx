aa1b3d337aa0a1f5d3192bb2a03c6e348bc6f5e6
Princedewangan101 <dewanganprince5151@gmail.com>
2025-09-27 01:34:47 +0530

    added brand component

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -4,7 +4,9 @@ import Leftsidebar from "../components/Leftsidebar";
 import Center from "../components/Center"
 import Rightsidebar from "../components/Rightsidebar"
 
+
 import bg from "../assets/bg.jpg";
+import Brand from '../components/Brand';
 
 
 const Homepage = () => {
@@ -15,32 +17,33 @@ const Homepage = () => {
 
 
         <div className='w-full h-screen  bg-cover bg-center' style={{ backgroundImage: `url(${bg})` }}>
+            <div className='w-full h-screen  bg-cover bg-center ' >
+                <div
+                    className='w-[80%] h-[var(--main_container-height)] border border-purple-800 rounded-md overflow-hidden  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-transparent backdrop-blur-sm'>
 
-            <div
-                className='w-[80%] h-[var(--main_container-height)] border border-purple-800 rounded-md overflow-hidden  absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-transparent backdrop-blur-sm'>
+                    <div className=" h-[var(--main_container-height)]  grid grid-cols-12 grid-rows-5 gap-0">
 
-                <div className=" h-[var(--main_container-height)]  grid grid-cols-12 grid-rows-5 gap-0">
-
-                    {selectedUser  ?
-                        (<>
-                            <div className="col-span-3 row-span-5 ">
-                                <Leftsidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
-                            </div>
-                            <div className="col-span-6 row-span-5 col-start-4 border-l border-r border-purple-700/25 ">
-                                <Center  user={selectedUser} />
-                            </div>
-                            <div className="col-span-3 row-span-5 col-start-10 ">
-                                <Rightsidebar />
-                            </div>
-                        </>)
-                        : (<>
-                            <div className="col-span-6 row-span-5 ">
-                                <Leftsidebar  setSelectedUser={setSelectedUser} />
-                            </div>
-                            <div className="col-span-6 row-span-5 ">
-                                <Leftsidebar />
-                            </div>
-                        </>)}
+                        {selectedUser ?
+                            (<>
+                                <div className="col-span-3 row-span-5 ">
+                                    <Leftsidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+                                </div>
+                                <div className="col-span-6 row-span-5 col-start-4 border-l border-r border-purple-700/25 ">
+                                    <Center user={selectedUser} />
+                                </div>
+                                <div className="col-span-3 row-span-5 col-start-10 ">
+                                    <Rightsidebar />
+                                </div>
+                            </>)
+                            : (<>
+                                <div className="col-span-6 row-span-5 ">
+                                    <Leftsidebar setSelectedUser={setSelectedUser} />
+                                </div>
+                                <div className="col-span-6 row-span-5 ">
+                                    <Brand />
+                                </div>
+                            </>)}
+                    </div>
                 </div>
             </div>
         </div>
---
+++ src/components/Brand.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import logo from '../assets/logo_icon.svg'
+import TypeIt from "typeit-react";
+
+
+
+const Brand = () => {
+    const [key, setKey] = useState(0);
+
+    useEffect(() => {
+
+        const interval = setInterval(() => {
+            setKey(prev => prev + 1); 
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <section className='h-full bg-slate-700/25 flex flex-col justify-center pb-15 items-center gap-5'>
+            <img src={logo} alt="" className='aspect-square w-30 ' />
+            {/* <h5 className=''>Chat with any one any time.</h5> */}
+
+
+            <div className="App font-bold text-xl">
+                <TypeIt key={key} options={{ speed: 100 }}>
+                    Chat with any one any time.
+                </TypeIt>
+            </div>
+
+
+        </section>
+
+    )
+}
+
+export default Brand
